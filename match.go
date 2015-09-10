ea9fc2326de4e99558c49eb16e85e7d24f508928
Zack Scholl <zack.scholl@gmail.com>
2015-09-10 09:44:08 -0400

    Almost ready

--- match.go
+++ match.go
@@ -5,32 +5,8 @@ import (
     "github.com/arbovm/levenshtein"
     "bufio"
     "os"
+    "strings"
 )
-
-
-func addToFile(s string) {
-    slen := len(s)
-    if slen <= 4 {
-       addToCache("four",s)
-    } else {
-        for i := 0; i <= slen-3; i ++ {
-            addToCache(s[i:i+3],s)
-        }
-    }
-}
-
-func addToCache(spartial string, s string) {
-    f, err := os.OpenFile("cache/" + spartial, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
-    if err != nil {
-        panic(err)
-    }
-
-    defer f.Close()
-
-    if _, err = f.WriteString(s); err != nil {
-        panic(err)
-    }
-}
 
 
 func generateHash(path string) {
@@ -40,12 +16,67 @@ func generateHash(path string) {
 	scanner.Split(bufio.ScanLines) 
   
   for scanner.Scan() {
-    addToFile(scanner.Text())
+    s := strings.Replace(scanner.Text(),"/","",-1)
+    partials, num := getPartials(s)
+    if num>2 {
+        for i := 0; i < num; i ++ {
+            addToCache(partials[i],s)
+        }
+    }
   }
 }
 
+
+func addToCache(spartial string, s string) {
+    f, err := os.OpenFile("cache/" + spartial, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+    if err != nil {
+        fmt.Println("%v",spartial)
+        panic(err)
+    }
+
+    defer f.Close()
+
+    if _, err = f.WriteString(s+"\n"); err != nil {
+        fmt.Println("%v",spartial)
+        panic(err)
+    }
+}
+
+func getPartials(s string) ([]string, int) { 
+    partials := make([]string,1000)
+    num := 0
+    s = strings.Replace(s,"/","",-1)
+    slen := len(s)
+    if slen <= 4 {
+       partials[num] = s
+       num = num + 1
+    } else {
+        for i := 0; i <= slen-3; i ++ {
+            partials[num] = s[i:i+3]
+            num = num + 1
+        }
+    }
+    return partials,num
+}
+
+func getMatch(s string) (string) {
+    partials, num := getPartials(s)
+    matches := make([]string,1000)
+    numm := 0
+    if num>2 {
+        for i := 0; i < num; i ++ {
+            // allPossibleMatches
+        }
+    }  
+    match := "No match"
+    return match
+}
+
+
 func main() {
-    generateHash("wordlist")
+    //generateHash("wordlist")
+    match := getMatch("aardvark")
+    fmt.Println("%v",match)
     s1 := "marcy playground"
     s2 := "mary playground"
     fmt.Printf("The distance between %v and %v is %v\n",
