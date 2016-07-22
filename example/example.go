419ec959723ca994b5389a3033394ecb55c2fb2a
Zack Scholl <zack.scholl@gmail.com>
2016-07-22 18:35:41 -0400

    Fixed to use 1.6 version

--- example/example.go
+++ example/example.go
@@ -7,21 +7,24 @@ import (
 	"github.com/schollz/goagrep/goagrep"
 )
 
-func init() {
-	// Build database
-	// only needs to be done once!
-	databaseFile := "words.db"
-	wordlist := "testlist"
-	tupleLength := 3
-	if _, err := os.Stat(databaseFile); os.IsNotExist(err) {
-		goagrep.GenerateDB(wordlist, databaseFile, tupleLength)
-	}
+var databaseFile string
+var wordlist string
+var tupleLength int
 
+func init() {
+	databaseFile = "words.db"
+	wordlist = "testlist"
+	tupleLength = 5
+
+	// Build database
+	if _, err := os.Stat(databaseFile); os.IsNotExist(err) {
+		goagrep.GenerateDB(wordlist, databaseFile, tupleLength, true)
+	}
 }
+
 func main() {
 	// Find word
-	databaseFile := "words.db"
 	searchWord := "heroint"
-	word, score := goagrep.GetMatch(searchWord, databaseFile)
-	fmt.Println(word, score)
+	word, score, err := goagrep.GetMatch(searchWord, databaseFile)
+	fmt.Println(word, score, err)
 }
